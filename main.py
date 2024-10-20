c2abfa842884532f523401271f896bb9726c871c
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-10-21 02:03:13 +0300

    adding the example with prints function to main file

---
+++ main.py
@@ -0,0 +1,55 @@
+from bfs import BFS
+
+def print_path(path):
+    # printing path for reaching the goal
+    print(f'Path to the goal:')
+    for arr in path:
+        for r in arr:
+            print(r)
+        print('*'*100)
+
+def print_expanded_nodes(expanded):
+    # printing number of visited nodes and the visited nodes itself 
+    print(f'Number of expanded nodes: {len(expanded)}')
+    print(f'Expanded nodes:')
+    for node in expanded:
+        print(node.get_empty_tile_location())
+        for r in node.get_board():
+            print(r)
+        print('*'*50)
+
+def print_forntier_nodes(frontier):
+    # printing number of in progressed nodes but not visited and the progressed nodes itself 
+    print(f'Number of not expanded nodes: {len(frontier)}')
+    print(f'Not expanded nodes:')
+    for node in frontier:
+        print(node.get_empty_tile_location())
+        for r in node.get_board():
+            print(r)
+        print('*'*50)
+
+
+def main():
+    # example
+    board = [
+        [1, 2, 5],
+        [3, 4, 0],
+        [6, 7, 8]
+    ]
+
+    bfs = BFS(board)
+
+    # perform BFS algorithm
+    path, frontier, expanded = bfs.solve()
+
+
+    print_path(path)
+    print("="*100)
+
+    print_expanded_nodes(expanded)
+    print("="*100)
+
+    print_forntier_nodes(frontier)
+
+
+main()
