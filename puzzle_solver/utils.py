ab9a62a2478162fb3f34a1bdd418e57c218b8f48
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-10-21 17:01:25 +0300

    Change the manhattan distance and euclidean distance to work on a board as a single int

--- puzzle_solver/utils.py
+++ puzzle_solver/utils.py
@@ -2,30 +2,42 @@
 from typing import List
 
 
-def manhattan_distance(board: List[int]) -> int:
+def manhattan_distance(board: int) -> int:
     """
     Calculates the manhattan distance for a given board.
     returns the distance.
     """
     cost = 0
-    for i in range(len(board)):
-        true_x, true_y = i // 3, i % 3
-        cur_x, cur_y = board[i] // 3, board[i] % 3
+    for i in range(9):
+        tile = (board // (10 ** i)) % 10  # Extracft the current tile
+        if tile == 0:  # Skip the empty tile
+            continue
+
+        # True position: tile
+        true_x, true_y = tile // 3, tile % 3
+        # Current position: 8 - i
+        cur_x, cur_y = (8 - i) // 3, (8 - i) % 3
 
         cost += abs(true_x - cur_x) + abs(true_y - cur_y)
 
     return cost
 
 
-def euclidean_distance(board: List[int]) -> float:
+def euclidean_distance(board: int) -> float:
     """
     Calculates the Euclidean distance for a given board.
     returns the distance.
     """
     cost = 0
-    for i in range(len(board)):
-        true_x, true_y = i // 3, i % 3
-        cur_x, cur_y = board[i] // 3, board[i] % 3
+    for i in range(9):
+        tile = (board // (10 ** i)) % 10  # Extracft the current tile
+        if tile == 0:  # Skip the empty tile
+            continue
+
+        # True position: tile
+        true_x, true_y = tile // 3, tile % 3
+        # Current position: 8 - i
+        cur_x, cur_y = (8 - i) // 3, (8 - i) % 3
 
         cost += ((true_x - cur_x) ** 2 + (true_y - cur_y) ** 2) ** 0.5
 
