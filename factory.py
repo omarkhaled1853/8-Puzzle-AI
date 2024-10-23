22d50469d1c99fc458daa442cce5889d97f7c191
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-10-24 01:26:49 +0300

    Adding factory class

---
+++ factory.py
@@ -0,0 +1,11 @@
+class Factory:
+    def get_technique(algorithm_name: str, intial_state: int, limit = None, heuristic = None):
+        if algorithm_name == 'BFS':
+            return BFS(intial_state)
+        elif algorithm_name == 'DFS':
+            return DFS(intial_state)
+        elif algorithm_name == 'IDFS':
+            return IDFS(intial_state, limit)
+        elif algorithm_name == 'A_star':
+            return A_Star(intial_state, heuristic)
+        else: None
