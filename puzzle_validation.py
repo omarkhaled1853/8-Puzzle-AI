358596cbd73eaf67720a4984f23358ecd9b3bdea
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-10-28 04:05:04 +0300

    Adding puzzle validation

---
+++ puzzle_validation.py
@@ -0,0 +1,23 @@
+def getInvCount(arr):
+	inv_count = 0
+	for i in range(0, 9):
+		for j in range(i + 1, 9):
+			if arr[j] != '0' and arr[i] != '0' and int(arr[i]) > int(arr[j]):
+				inv_count += 1
+	return inv_count
+
+# check if solvable puzzle
+def isSolvable(puzzle) :
+
+	# Count inversions
+	inv_count = getInvCount(puzzle)
+
+	# return true if inversion count is even.
+	return (inv_count % 2 == 0)
+
+# example
+puzzle = '812043765'
+if(isSolvable(puzzle)) :
+	print("Solvable")
+else :
+	print("Not Solvable")
