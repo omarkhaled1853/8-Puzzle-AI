e0956f36f38caa30b374e36cde18d3dce7892fb8
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-10-28 16:44:20 +0300

    Finish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from PuzzleGUI.puzzle_gui import *
 
-
-root = tk.Tk()
-Puzzle(root)
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    Puzzle(root)
+    root.mainloop()
